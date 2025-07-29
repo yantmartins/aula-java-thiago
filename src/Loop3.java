fcd8fdd9ee4b4b26d5b56f43991d312ef7baf73c
Yan <yantmartins@live.com>
2025-07-28 20:10:16 -0400

    up exercicios

---
+++ src/Loop3.java
@@ -0,0 +1,8 @@
+public class Loop3 {
+    public static void main(String[] args) {
+        
+        for(int x = 100; x >= 0; x--){
+            System.out.println("Valor de X: " + x);
+        }
+    }
+}
---
+++ src/Teste4.java
@@ -0,0 +1,16 @@
+public class Teste4 {
+    public static void main(String[] args) {
+        
+        int num = 10;
+        num++;
+        num++;
+        num++;
+        num++;
+        num++;
+        num++;
+        System.out.println(num);
+        num--;
+        num--;
+        System.out.println(num);
+    }
+}
