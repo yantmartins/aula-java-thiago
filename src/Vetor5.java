76a3a551954e9c86dbeb6a58bf8640f7aa0177ff
Yan <yantmartins@live.com>
2025-07-30 20:02:18 -0400

    up vetores

---
+++ src/Vetor5.java
@@ -0,0 +1,8 @@
+public class Vetor5 {
+    public static void main(String[] args) {
+        String[] mes = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+        int[] diasMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    }
+}
---
+++ src/Vetor6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Vetor6 {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        int n = 5;
+        int vet[] = new int[n];
+        int i;
+
+        for(i = 0; i < n; i++){
+            System.out.printf("Informe  2%do. valor de %d: ", (i + 1), n);
+            vet[i] = entrada.nextInt();
+        }
+
+        entrada.close();
+    }
+}
